e5f217ed2a65a8801de2a18c057cc8dc27038c78
Your Name <nguyenson120296@gmail.com>
2020-07-26 17:11:43 +0700

    setup data for last access date

--- app/src/main/java/com/sdt/fossilhometest/utils/DateUtils.java
+++ app/src/main/java/com/sdt/fossilhometest/utils/DateUtils.java
@@ -1,4 +1,20 @@
 package com.sdt.fossilhometest.utils;
 
-class DateUtils {
+import android.text.format.DateFormat;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+public final class DateUtils {
+
+    public static final String BASE_DATE = "dd-MM-yyyy";
+
+    public static String getBaseDate(long time) {
+        Calendar cal = Calendar.getInstance(Locale.ENGLISH);
+        cal.setTimeInMillis(time * 1000);
+        return DateFormat.format(BASE_DATE, cal).toString();
+    }
+
+    private DateUtils() {}
+
 }
